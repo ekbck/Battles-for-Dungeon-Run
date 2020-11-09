8a6971889dbf73aa675c760b4247dc961872654e
fabia <fabia@Asteria.lan>
2020-11-09 16:33:33 +0100

    Klar strid

--- src/battles/Battles.java
+++ src/battles/Battles.java
@@ -4,133 +4,123 @@ import java.util.Scanner;
 
 public class Battles {
 
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        // Spelaren kommer in i rummet
-        // Spelaren slår tärning lika många gånger som hjältens Initiativ och totala värdet sparas
-        // Monstren i rummet slår tärning lika många gånger som deras Initiativ och totala värdet per monster sparas
-        // Alla Initiativ-värden jämförs och den som får högst börjar, sen går det i fallande ordning
-        // När det är spelarens tur får den först välja om den vill strida eller fly
-        // Vill spelaren strida så kastas lika många tärningar som hjältens Attack
-        // Monstret slår lika många tärningar som dess Smidighet
-        // Spelarens Attack-värde jämförs mot monstrets Smidighets-värde
-        // Är Attack-värdet STÖRRE än Smidighet-värdet så dras ett poäng från monstrets Tålighet
-        // Är Attack-värdet MINDRE än Smidighet-värdet så missar attacken.
-        // Vill spelaren fly så gångras dennes Smidighet med 10 för att räkna ut hur stor procents chans denne har att fly
-        // Lyckas spelaren fly går denne tillbaka till föregående rum och får välja var denne ska gå näst
-        // Spelet måste komma ihåg monstren som var i rummet
-        // Lyckas spelaren inte fly går turen över till monstret som attackerar
-        // När det är monstrets tur attackerar det, då jämförs monstrets Attack-värde mot spelaren Smidighet-värde
-        // Striden tar slut när spelaren vinner över alla monster, själv dör eller lyckas fly
-        battle();
+        //Hero
+        int heroHP = 9; // hero.getHealth
+        int heroInit = 5; // hero.getInitiative
+        int heroAgility = 4; // hero.getAgility
+        int choice;
+        int hAttack;
+        int flee = 0;
+
+        //Monster
+        int monsterHP = 1; // monster.getHealth
+        int monsterInit = 7; // monster.getInitiative
+        int mAttack;
+
+        System.out.println("There is a monster in the room!");
+
+        int whoToStart = checkInitiative(heroInit, monsterInit);
+
+        if (whoToStart == 1) {
+            while (flee != 1 && heroHP >= 1 && monsterHP >= 1) {
+                System.out.println("\nYOUR HP: " + heroHP + "\nMONSTER'S HP: " + monsterHP);
+                choice = fightOrFlight();
+                if (choice == 1) {
+                    hAttack = heroAttack();
+                    if (hAttack == 1) {
+                        monsterHP--; // monster.setHealth??
+                    }
+                    mAttack = monsterAttack();
+                    if (mAttack == 1) {
+                        heroHP--; // hero.setHealth??
+                    }
+                } else if (choice == 2) {
+                    flee = flee(heroAgility);
+                    if (flee != 1) {
+                        mAttack = monsterAttack();
+                        if (mAttack == 1) {
+                            heroHP--;
+                        }
+                    }
+                }
+            }
+        } else {
+            while (flee != 1 && heroHP >= 1 && monsterHP >= 1) {
+                mAttack = monsterAttack();
+                if (mAttack == 1) {
+                    heroHP--;
+                }
+                System.out.println("\nYOUR HP: " + heroHP + "\nMONSTER'S HP: " + monsterHP);
+                choice = fightOrFlight(); 
+                if (choice == 1) {
+                    hAttack = heroAttack();
+                    if (hAttack == 1) {
+                        monsterHP--;
+                    }
+                } else if (choice == 2) {
+                    flee = flee(heroAgility);
+                }
+            }
+        }
+        
+        if (heroHP <= 0) {
+            System.out.println("\nYou died!");
+        } else if (monsterHP <= 0) {
+            System.out.println("\nYou defeated the beast!");
+        }
+
     }
 
-    public static void battle() {
-        Scanner sc = new Scanner(System.in);
+    public static int checkInitiative(int hero, int monster) {
 
-        System.out.println("There is a monster in the room! \n1. Fight! \n2. Escape");
+        int heroInitSum = rollDice(hero);
+        int monsterInitSum = rollDice(monster);
 
+        System.out.println("\nYour initiative is: " + heroInitSum);
+        System.out.println("The monster's initiative is: " + monsterInitSum);
+
+        if (heroInitSum > monsterInitSum) {
+            System.out.println("You start");
+            return 1;
+        } else {
+            System.out.println("The monster starts attacking you");
+            return 0;
+        }
+
+    }
+
+    public static int fightOrFlight() {
+        System.out.println("\nChoose: \n1. Fight \n2. Flee");
         int choice = sc.nextInt();
         sc.nextLine();
 
-        switch (choice) {
-            case 1:
-                int heroHP = 9;
-                int monsterHP = 1;
-
-                System.out.println("\nYou chose to fight!");
-
-                int heroInitiative = 5; // Riddarens Initiativ. Här ska man egentligen hämta just den valda Hjältens Initiativ
-                int heroInitiativeSum = rollDice(heroInitiative);
-                System.out.println("Your initiative is: " + heroInitiativeSum);
-
-                int monsterInitiative = 7; // Spindelns Initiativ. Här ska man egentligen hämta just det slumpade Monstrets Initiativ
-                int monsterInitiativeSum = rollDice(monsterInitiative);
-                System.out.println("The monster's initiative is: " + monsterInitiativeSum);
-
-                if (heroInitiativeSum > monsterInitiativeSum) {
-
-                    System.out.println("\nThis means that you get to start attacking!");
-
-                    while (heroHP >= 1 && monsterHP >= 1) {
-
-                        if (heroHP >= 1) {
-                            int heroAttack = heroAttack();
-                            if (heroAttack == 1) {
-                                monsterHP--;
-                            }
-                        }
-
-                        if (monsterHP >= 1) {
-                            int monsterAttack = monsterAttack();
-                            if (monsterAttack == 1) {
-                                heroHP--;
-                            }
-                        }
-                    }
-                } else {
-
-                    System.out.println("\nThis means that the monster starts attacking you!");
-
-                    while (heroHP >= 1 && monsterHP >= 1) {
-
-                        if (monsterHP >= 1) {
-                            int monsterAttack = monsterAttack();
-                            if (monsterAttack == 1) {
-                                heroHP--;
-                            }
-                        }
-
-                        if (heroHP >= 1) {
-                            int heroAttack = heroAttack();
-                            if (heroAttack == 1) {
-                                monsterHP--;
-                            }
-                        }
-                    }
-
-                }
-                if (heroHP <= 0) {
-                    System.out.println("\n+          +");
-                    System.out.println("| You died |");
-                }
-                if (monsterHP <= 0) {
-                    System.out.println("\nYOU DEFEATED THE BEAST!!");
-                }
-
-                break;
-
-            case 2:
-                System.out.println("You chose to try to escape!");
-                break;
-
-            default:
-                System.out.println("Error! \nPlease choose 1 or 2!");
-                break;
+        if (choice == 1) {
+            return 1;
+        } else if (choice == 2) {
+            return 2;
+        } else {
+            return 0;
         }
-
     }
 
-    public static int rollDice(int numberOfDice) {
-        int sum = 0;
+    public static int flee(int flee) {
+        System.out.println("\nYou try to escape...");
 
-        for (int i = 0; i < numberOfDice; i++) {
+        int chance = flee * 10;
 
-            int resultOfDie = (int) (Math.random() * 6) + 1; // Slår en tärning med maxvärde 6
-            sum += resultOfDie; // Lägger ihop alla träningsresultat
+        int result = (int) (Math.random() * 100) + 1; // Drar random tal mellan 1 - 100
+
+        if (result <= chance) {
+            System.out.println("and you manage to get away!");
+            return 1;
+        } else {
+            System.out.println("but to no avail, you have to stay and fight one more round!");
+            return 0;
         }
-        return sum;
-
-    }
-
-    public static int attack(int attack) {
-        int attackSum = rollDice(attack);
-        return attackSum;
-    }
-
-    public static int dodge(int inititative) {
-        int initiativeSum = rollDice(inititative);
-        return initiativeSum;
     }
 
     public static int heroAttack() {
@@ -148,7 +138,6 @@ public class Battles {
             System.out.println("So you miss the monster...");
             return 0;
         }
-
     }
 
     public static int monsterAttack() {
@@ -166,6 +155,27 @@ public class Battles {
             System.out.println("So the monster swings and misses you!");
             return 0;
         }
+    }
+
+    public static int attack(int attack) {
+        int attackSum = rollDice(attack);
+        return attackSum;
+    }
+
+    public static int dodge(int inititative) {
+        int initiativeSum = rollDice(inititative);
+        return initiativeSum;
+    }
+
+    public static int rollDice(int numberOfDice) {
+        int sum = 0;
+
+        for (int i = 0; i < numberOfDice; i++) {
+
+            int resultOfDie = (int) (Math.random() * 6) + 1; // Slår en tärning med maxvärde 6
+            sum += resultOfDie; // Lägger ihop alla tärningsresultat
+        }
+        return sum;
     }
 
 }
